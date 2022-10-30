caa67b80ae2333604bf1f81cb1f11a49ce19969b
Ricardo Asin <71453150+ricardoasinr@users.noreply.github.com>
2022-10-30 12:22:19 -0400

    Creando el RandomController

--- Backend/Ayudantia28WebApp/Ayudantia28WebApp/Controllers/RandomController.cs
+++ Backend/Ayudantia28WebApp/Ayudantia28WebApp/Controllers/RandomController.cs
@@ -11,49 +11,21 @@ namespace Ayudantia28WebApp.Controllers
     public class RandomController : ControllerBase
     {
         private readonly AppDbContext _context;
-
-
-
-
-
-
-
-        public RandomController(AppDbContext context)
-
-        {
-
+        public RandomController(AppDbContext context){
             _context = context;
-
         }
 
         [HttpGet]
-
-        public async Task<ActionResult<People>> GetFuturos()
-
+        public async Task<ActionResult<People>> GetFuturos() //Donde está <People> ahi entra nuestro Model que estemos utilizando
         {
-
             var list = await _context.people.ToListAsync();
-
-
-
             var max = list.Count;
-
             int index = new Random().Next(0, max);
-
             var people = list[index];
-
-
-
-            if (people == null)
-
-            {
-
+            if (people == null){
                 return NoContent();
-
             }
-
-
-
+            
             return people;
 
         }
